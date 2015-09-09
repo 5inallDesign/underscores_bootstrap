d6294718f1f9992d8eeb840ed03400545e4d188e
Matt Crandell <mrcrandell@gmail.com>
2015-09-08 20:53:30 -0400

    Added files and templates to create a basic theme.

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Underscores and Bootstrap
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscores_bootstrap' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'underscores_bootstrap' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscores_bootstrap' ), 'underscores_bootstrap', '<a href="http://5inalldesign.com" rel="designer">Matt Crandell</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
